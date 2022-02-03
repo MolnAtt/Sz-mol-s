56de788841b821b4cda4891339b6bbc911b7c148
Molnár Attila <molnar.attila@szlgbp.hu>
2022-02-03 16:37:00 +0100

    Add project files.

---
+++ Számolás/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Számolás
+{
+    class Program
+    {
+
+        static Random g = new Random();
+        static void Main(string[] args)
+        {
+            var t =  Számolás("a", 10);
+
+            t.Wait();
+
+            Console.ReadLine();
+        }
+
+        async static Task<bool> Számolás(string l, int N)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"{l}{i + 1}");
+                await Task.Delay(200 + g.Next(800));
+            }
+            return true;
+        }
+    }
+}
