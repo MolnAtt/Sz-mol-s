322b53535b0fd236b7903d7921a9622d3a90253d
Molnár Attila <molnar.attila@szlgbp.hu>
2022-02-03 16:42:00 +0100

    két különböző tempóban haladó számsorozat

--- Számolás/Program.cs
+++ Számolás/Program.cs
@@ -12,16 +12,18 @@ namespace Számolás
         static Random g = new Random();
         static void Main(string[] args)
         {
-            var t =  Számolás("a", 10);
+            var t1 = Számolás("a", 20, 2);
+            var t2 = Számolás("b", 20, 1);
 
-            t.Wait();
+            t1.Wait();
+            t2.Wait();
 
             Console.ReadLine();
         }
 
-        async static Task<bool> Számolás(string l, int N)
+        async static Task<bool> Számolás(string l, int N, int k)
         {
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < N; i+=k)
             {
                 Console.WriteLine($"{l}{i + 1}");
                 await Task.Delay(200 + g.Next(800));
